618b75ebba79a5fa63d3452ae0eefa3760a8c7ff
Karen Rubio <120537042+KarenRubio@users.noreply.github.com>
2024-03-02 04:00:17 +0000

    Funcionalidades primarias agregadas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,53 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numCard = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let nipe = ["diamond", "spade", "heart", "club"];
+  let randomNumCard = numCard[Math.floor(Math.random() * numCard.length)];
+  let randomNipe = nipe[Math.floor(Math.random() * nipe.length)];
+
+  if (randomNipe === "heart" || randomNipe === "diamond") {
+    let iconTop = document.createElement("i");
+    iconTop.classList.add("bi", `bi-suit-${randomNipe}-fill`, "text-danger");
+
+    let iconBottom = document.createElement("i");
+    iconBottom.classList.add("bi", `bi-suit-${randomNipe}-fill`, "text-danger");
+
+    document.querySelector(".top").innerHTML = "";
+    document.querySelector(".top").appendChild(iconTop);
+    document.querySelector(".bottom").innerHTML = "";
+    document.querySelector(".bottom").appendChild(iconBottom);
+  } else {
+    let iconTop = document.createElement("i");
+    iconTop.classList.add("bi", `bi-suit-${randomNipe}-fill`);
+    let iconBottom = document.createElement("i");
+    iconBottom.classList.add("bi", `bi-suit-${randomNipe}-fill`);
+
+    document.querySelector(".top").innerHTML = "";
+    document.querySelector(".top").appendChild(iconTop);
+    document.querySelector(".bottom").innerHTML = "";
+    document.querySelector(".bottom").appendChild(iconBottom);
+  }
+
+  document.getElementById("num").textContent = randomNumCard;
 };
+
+//hacer fondo verde
+// crear una card blanca
+//graficar la card con mazo y numero
+//hacer que sea random el mazo y numero
